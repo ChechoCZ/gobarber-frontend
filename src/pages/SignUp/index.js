512939212fa3d9fcd7438023a8a427586468ca74
Sergio Cuadros <sergio@codwise.com>
2020-03-21 15:12:26 -0300

    feat: Initial app styling and routing set up.

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name required'),
+  email: Yup.string().email('Invalid e-mail').required('E-mail required'),
+  password: Yup.string().min(6, 'Password should have at least 6 characters').required('Password required')
+});
+
+export default function SignUp() {
+  function handleSubmit(data) {
+
+  }
+
+  return (
+    <>
+      <img src={ logo } alt="GoBarber"/>
+
+      <Form schema={ schema } onSubmit={ handleSubmit }>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your secret password" />
+
+        <button type="submit">Create account</button>
+
+        <Link to="/">I'm already registered!</Link>
+      </Form>
+    </>
+  );
+}
---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  background: linear-gradient(-9deg, #7159c1, #ab59c1);
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    margin-top: 30px;
+    flex-direction: column;
+
+    input {
+      border: 0;
+      color: #FFF;
+      height: 44px;
+      padding: 0 15px;
+      margin: 0 0 10px;
+      border-radius: 4px;
+      background: rgba(0, 0, 0, 0.1);
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    span {
+      color: #FFF;
+      margin: 0 0 10px;
+      align-self: flex-start;
+    }
+
+    button {
+      border: 0;
+      color:#FFF;
+      height: 44px;
+      margin: 5px 0 0;
+      font-size: 16px;
+      font-weight: bold;
+      border-radius: 4px;
+      background: #3B9EFF;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#3B9EFF')};
+      }
+    }
+
+    a {
+      color: #FFF;
+      opacity: 0.8;
+      font-size: 16px;
+      margin-top: 15px;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
